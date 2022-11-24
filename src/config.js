f63c83176619fbf7ab119b38c654f5a18bd4e65d
BenjaminCarpio <00205619@uca.edu.sv>
2022-11-23 20:07:42 -0600

    Change config 2

--- src/config.js
+++ src/config.js
@@ -5,7 +5,8 @@ const config = {
                 {
                     origin: "localhost:3000", //servidor que deseas que consuma o (*) en caso que sea acceso libre
                     credentials: true,
-                    origin: "https://athenapi.herokuapp.com/api/reviews"
+                    origin: "https://athenapi.herokuapp.com/api/reviews",
+                    credentials: true
                 }
             ]
         }
