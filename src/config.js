8f30592cde153a4ea63cf832ce331d8a18b935cc
unknown <00225919@uca.edu.sv>
2022-11-23 21:31:51 -0600

    hola

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ const config = {
             server: [
                 {
                     origin: "localhost:3000", //servidor que deseas que consuma o (*) en caso que sea acceso libre
-                    credentials: true,
+                    credentials: false,
                     origin: "https://athenapi.herokuapp.com/api/reviews",
-                    credentials: true
+                    credentials: false
                 }
             ]
         }
