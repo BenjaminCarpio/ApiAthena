a0dc0625154c1e50ac0bca3134ea51895fd9446a
BenjaminCarpio <00205619@uca.edu.sv>
2022-11-23 19:53:02 -0600

    Change config js

---
+++ src/config.js
@@ -0,0 +1,15 @@
+const config = {
+    application: {
+        cors: {
+            server: [
+                {
+                    origin: "localhost:3000", //servidor que deseas que consuma o (*) en caso que sea acceso libre
+                    credentials: true
+                }
+            ]
+        }
+}
+}
+
+////a holaaas 
+///detectame fuch
