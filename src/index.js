c9426136f9d89632a5d7dae6d1efd76aa3862b70
unknown <00225919@uca.edu.sv>
2022-11-23 22:08:18 -0600

    cors1;

--- src/index.js
+++ src/index.js
@@ -41,14 +41,18 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
   next()
-});
-app.get('/', function(req, res, next) {
-  // Handle the get for this route
-});
-app.post('/', function(req, res, next) {
-  // Handle the post for this route
 })
